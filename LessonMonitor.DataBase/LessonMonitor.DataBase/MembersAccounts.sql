d2be8206c7533f2b4368f76f00fc58d440504216
Desk Any <combinerk@gmail.com>
2022-06-12 21:50:20 +0300

    Добавил таблицу MembersAccounts

---
+++ LessonMonitor.DataBase/LessonMonitor.DataBase/MembersAccounts.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[MembersAccounts]
+(
+	[GitHub] NCHAR,
+	[Youtube] NCHAR,
+	[MemberId] INT NOT NULL CONSTRAINT FkMember FOREIGN KEY (MemberId) REFERENCES dbo.Members(MemberId)
+)
